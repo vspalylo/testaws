9b327f4c079bb0ce8d11b4471cacce5cab372925
vspalylo <git@github.com>
2021-02-14 20:56:11 +0200

    test/test1.c test/test2.c
    :wq

---
+++ test/test1.c
@@ -0,0 +1,16 @@
+//addition program
+#include <stdio.h>
+
+main()
+{
+    int integer1, integer2, sum;
+    printf("Enter first integer\n");
+    scanf("%d", &integer1);
+    printf("Enter second integer\n");
+    scanf("%d", &integer2);
+    sum = integer1 + integer2;
+    printf("Sum is %d\n", sum);
+
+    return 0;
+}
+
